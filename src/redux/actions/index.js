edd8b3736a860717b70e0caac48dfd93bc3e1f7b
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-04 17:17:10 -0300

    configuração das actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
-// Coloque aqui suas actions
+export const USER_INFO = 'USER_INFO';
+export const WALLET_DATA = 'WALLET_DATA';
+
+const updateData = (type, state) => ({
+  type,
+  payload: { ...state },
+});
+
+export default updateData;

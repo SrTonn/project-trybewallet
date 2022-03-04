550cc3aee638b5c58da1c77dccd2b5bce756b279
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-04 17:17:43 -0300

    configuracao da wallet

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET_DATA } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  if (action.type === WALLET_DATA) {
+    // todo: corrigir essa lógica quando souber como será feito o dispatch
+    return {
+      currencies: action.payload,
+      expenses: action.payload,
+    };
+  }
+  return state;
+};
+
+export default wallet;

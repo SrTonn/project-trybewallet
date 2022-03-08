d135401c3ffe1240258697628da7664311e91335
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-08 16:08:54 -0300

    rotas definidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
     );
   }
 }

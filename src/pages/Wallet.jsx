969fbb9cdfa4160a7aa54a9f6f123edc49f6fb11
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-08 17:06:43 -0300

    requisito 03 concluido

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">
+          Email:&nbsp;
+          <span>{email}</span>
+        </p>
+        <p>
+          Despesa Total: R$&nbsp;
+          <span data-testid="total-field">0.00</span>
+        </p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = ({ user }) => ({
+  ...user,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};

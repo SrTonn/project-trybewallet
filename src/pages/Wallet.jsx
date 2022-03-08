4620bd70bb141df76556ca2f7a0c52c428fd566f
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-08 18:04:31 -0300

    correcao de indentacao

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import FormExpensive from '../components/FormExpensive';
 
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
       <>
-      <header>
-        <p data-testid="email-field">
-          Email:&nbsp;
-          <span>{email}</span>
-        </p>
-        <p>
-          Despesa Total: R$&nbsp;
-          <span data-testid="total-field">0.00</span>
-        </p>
-        <p data-testid="header-currency-field">BRL</p>
-      </header>
+        <header>
+          <p data-testid="email-field">
+            Email:&nbsp;
+            <span>{email}</span>
+          </p>
+          <p>
+            Despesa Total: R$&nbsp;
+            <span data-testid="total-field">0.00</span>
+          </p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
 
         <FormExpensive />
       </>

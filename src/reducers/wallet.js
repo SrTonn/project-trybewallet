998d80a41accab19758e156f0724bc3d6b4fe09b
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-10 21:06:37 -0300

    add opcao edit nas actions

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,10 @@
-import { WALLET_CURRENCIES, WALLET_EXPENSES, WALLET_EXPENSES_DELETE } from '../actions';
+import {
+  IS_EDIT_MODE_ON,
+  WALLET_CURRENCIES,
+  WALLET_EXPENSES,
+  WALLET_EXPENSES_DELETE,
+  WALLET_EXPENSES_EDIT,
+} from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -27,6 +33,28 @@ const wallet = (state = INITIAL_STATE, action) => {
     };
   }
 
+  if (action.type === WALLET_EXPENSES_EDIT) {
+    const { exchangeRates } = state.expenses.find(({ id }) => id === +action.payload.id);
+    const editedObj = {
+      ...action.payload,
+      exchangeRates,
+    };
+
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses.filter(({ id }) => id !== +action.payload.id),
+        editedObj,
+      ].sort((a, b) => a.id - b.id),
+    };
+  }
+
+  if (action.type === IS_EDIT_MODE_ON) {
+    return {
+      ...state,
+      editRow: action.payload,
+    };
+  }
   return state;
 };
 

55b5be11086045066d72592ec600296e9d376a2a
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-10 22:29:06 -0300

    remocao das tags table|tgead|tbody

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -22,54 +22,49 @@ class Table extends Component {
   render() {
     const { expenses } = this.props;
     return (
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map(
-            ({ id, currency, description, tag, method, value, exchangeRates }) => (
-              <tr key={ id + description }>
-                <td>{description || '...'}</td>
-                <td>{tag}</td>
-                <td>{method}</td>
-                <td>{(+value || 0).toFixed(2)}</td>
-                <td>{exchangeRates[currency].name.replace('/Real Brasileiro', '')}</td>
-                <td>{(+exchangeRates[currency].ask).toFixed(2)}</td>
-                <td>{(exchangeRates[currency].ask * value).toFixed(2)}</td>
-                <td>Real</td>
-                <td>
-                  <Button
-                    label="Editar"
-                    name="editar"
-                    dataTestId="edit-btn"
-                    handleClick={ this.handleClick }
-                    id={ id }
-                  />
-                  <Button
-                    label="Excluir"
-                    name="excluir"
-                    dataTestId="delete-btn"
-                    handleClick={ this.handleClick }
-                    id={ id }
-                  />
-                </td>
-              </tr>
-            ),
-          )}
-
-        </tbody>
-      </table>
+      <>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        {expenses.map(
+          ({ id, currency, description, tag, method, value, exchangeRates }) => (
+            <tr key={ id + description }>
+              <td>{description || '...'}</td>
+              <td>{tag}</td>
+              <td>{method}</td>
+              <td>{(+value || 0).toFixed(2)}</td>
+              <td>{exchangeRates[currency].name.replace('/Real Brasileiro', '')}</td>
+              <td>{(+exchangeRates[currency].ask).toFixed(2)}</td>
+              <td>{(exchangeRates[currency].ask * value).toFixed(2)}</td>
+              <td>Real</td>
+              <td>
+                <Button
+                  label="Editar"
+                  name="editar"
+                  dataTestId="edit-btn"
+                  handleClick={ this.handleClick }
+                  id={ id }
+                />
+                <Button
+                  label="Excluir"
+                  name="excluir"
+                  dataTestId="delete-btn"
+                  handleClick={ this.handleClick }
+                  id={ id }
+                />
+              </td>
+            </tr>
+          ),
+        )}
+      </>
     );
   }
 }
